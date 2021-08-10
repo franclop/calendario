b07b2741e9e1735bb05230f3f403db790cb81db3
franclop <88738734+franclop@users.noreply.github.com>
2021-08-10 15:40:32 -0500

    Add files via upload

---
+++ Calendario/Calendario/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Calendario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bMod = new System.Windows.Forms.Button();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bEli = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bMod
+            // 
+            this.bMod.Location = new System.Drawing.Point(666, 126);
+            this.bMod.Name = "bMod";
+            this.bMod.Size = new System.Drawing.Size(149, 51);
+            this.bMod.TabIndex = 0;
+            this.bMod.Text = "Modificar evento";
+            this.bMod.UseVisualStyleBackColor = true;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(666, 12);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(149, 51);
+            this.bAdd.TabIndex = 1;
+            this.bAdd.Text = "Agregar Fecha a evento";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // bEli
+            // 
+            this.bEli.Location = new System.Drawing.Point(666, 183);
+            this.bEli.Name = "bEli";
+            this.bEli.Size = new System.Drawing.Size(149, 51);
+            this.bEli.TabIndex = 2;
+            this.bEli.Text = "Eliminar evento";
+            this.bEli.UseVisualStyleBackColor = true;
+            this.bEli.Click += new System.EventHandler(this.bEli_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 22);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(311, 26);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre del evento";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(389, 45);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(251, 26);
+            this.tb1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fecha";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(389, 108);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(251, 26);
+            this.tb2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(385, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Categorias";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Vacaciones",
+            "Trabajo"});
+            this.comboBox1.Location = new System.Drawing.Point(389, 168);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(251, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 51);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Crear evento";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 363);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(515, 28);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Eventos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(560, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(207, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Agregar o eliminar categoria";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 43);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(564, 363);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 26);
+            this.textBox1.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(666, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 51);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Eliminar categoria";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 507);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.bEli);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.bMod);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bMod;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button bEli;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Calendario/Calendario/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            tb2.Text = dateTimePicker1.Value.ToLongDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nomEvento = tb1.Text;
+            string fecha = tb2.Text;
+            string cat = comboBox1.Text;
+            string evento = tb1.Text + " " + tb2.Text + " " + comboBox1.Text;
+
+            comboBox2.Items.Add(evento);
+
+            tb1.Text = "";
+            tb2.Text = "";
+            comboBox1.Text="";
+
+
+        }
+
+        private void bEli_Click(object sender, EventArgs e)
+        {
+            comboBox2.Items.Remove(comboBox2.SelectedItem);
+
+            comboBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string categoria = textBox1.Text;
+
+            comboBox1.Items.Add(categoria);
+
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
+
+            comboBox2.Text = "";
+        }
+    }
+}
